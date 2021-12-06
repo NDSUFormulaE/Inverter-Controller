93eaa953f30f1af464ff70733f936afe6a0e3a51
Brennan Coslett <brennan@coslett.org>
2021-12-05 19:28:56 -0600

    add tp test script

---
+++ send_tp.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+#while true
+#do
+cansend can0 "1CECFFA2#200A0002FFCAFE00" 
+sleep 0.015
+cansend can0 "1CEBFFA2#01FFFFA7FAE81FA2"
+sleep 0.015
+cansend can0 "1CEBFFA2#02E3E9B9FFFFFFFFF"
+sleep 0.015
+#done
