e3c2c0e462baec62cb36864cbfe0268f95ad1f6d
Brennan Coslett <brennan@coslett.org>
2021-12-08 09:45:14 -0600

    add initial control loop for state machine

--- InverterApp/InverterApp.ino
+++ InverterApp/InverterApp.ino
@@ -6,6 +6,7 @@
 //// CAN library and PD400 definitions
 #include "src/ARD1939/CAN_SPEC/PGN.h"
 #include "src/ARD1939/CAN_SPEC/StateTransition.h"
+#include "src/ARD1939/CAN_SPEC/MotorControlUnitState.h"
 
 //// Subsystem imports
 #include "src/gpioHandler/gpioHandler.h"
@@ -32,8 +33,37 @@ void setup()
         delay(500);
         resetFunc(); // If CAN Controller doesnt init correctly, wait 500ms then try again.
     }
-    taskMan.DisableDriveMessage();
     Serial.println("Initialized");
+    Serial.println("Attempting to move Inverter to Ignition Ready");
+    while(InverterState.MCU_State != MCU_IGNIT_READY)
+    {
+        if(InverterState.MCU_State == MCU_FAULT_CLASSA || InverterState.MCU_State == MCU_FAULT_CLASSA)
+        {
+          taskMan.ClearInverterFaults();
+          Serial.println("In Fault State");
+        }
+        else
+        {
+          taskMan.ChangeState(STDBY_TO_IGNIT_READY, INVERTER_CMD_MESSAGE_INDEX);      
+        }
+
+        loop();
+    }
+    Serial.println("Inverter State Ignition Ready");
+    Serial.println("Connect HVDC");
+    while(InverterState.MCU_State != MCU_PWR_READY)
+    {
+      loop();
+    }
+    Serial.println("Inverter State Power Ready");
+    while(InverterState.MCU_State != MCU_ADV_DIAG_CLASSB)
+    {
+        taskMan.ChangeState(PWR_READY_TO_ADV_DIAG_CLASSB, INVERTER_CMD_MESSAGE_INDEX);
+        loop();
+    }
+    Serial.println("Inverter State Adv Diag Class B");
+    //taskMan.ChangeState(DIAG_AD_B_MOTOR_POS_SENS_CALIB, INVERTER_CMD_MESSAGE_INDEX);
+    
 }
 
 void loop()
