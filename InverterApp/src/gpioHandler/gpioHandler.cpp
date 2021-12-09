bf796d4e0e2fb34a6d92839011714a3453279627
Shad0w-Dragon <91845760+Shad0w-Dragon@users.noreply.github.com>
2021-12-09 17:53:19 -0600

    Added display Initialization Function (#24)

--- InverterApp/src/gpioHandler/gpioHandler.cpp
+++ InverterApp/src/gpioHandler/gpioHandler.cpp
@@ -1,11 +1,29 @@
 #include "gpioHandler.h"
+#include "TM1637TinyDisplay.h"
+uint16_t speed = 0;
+
+
+bool GPIOHandler::Init()
+{
+    // Configure all of our GPIOs
+    #ifdef DISPLAYS_ENABLED
+        // ALL OF OUR CODE
+        TM1637TinyDisplay speedDisplay(22, 24), batteryDisplay(26,28), motorTempDisplay(30,32), coolantTempDisplay(34,36);
+        speedDisplay.setBrightness(7);
+        batteryDisplay.setBrightness(7);
+        coolantTempDisplay.setBrightness(7);
+        motorTempDisplay.setBrightness(7);
+        speedDisplay.showString("NDSU SAE");
+        speedDisplay.showNumber(00.00);
+        batteryDisplay.showNumber(000.0);
+        coolantTempDisplay.showNumber(000.0);
+        motorTempDisplay.showNumber(000.0);
+
+    #else
+        return true;
+    #endif
 
 unsigned long last_clear = 0;
-
-bool GPIOHandler::Init(void)
-{
-    return true;
-}
 
 uint16_t GPIOHandler::GetPedalSpeed()
 {
