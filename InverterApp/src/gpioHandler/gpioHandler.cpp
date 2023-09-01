4fbfff5524d49a98f75c966e836805d9c5611c97
Brennan Coslett <brennan@coslett.org>
2023-08-31 19:01:09 -0500

    Working LCD

--- InverterApp/src/gpioHandler/gpioHandler.cpp
+++ InverterApp/src/gpioHandler/gpioHandler.cpp
@@ -1,11 +1,20 @@
 #include "gpioHandler.h"
+#include "lcd.h" 
 #include "../TM1637TinyDisplay/TM1637TinyDisplay.h"
+#include "../LiquidCrystal_I2C/LiquidCrystal_I2C.h"
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#define printByte(args)  write(args);
+#else
+#define printByte(args)  print(args,BYTE);
+#endif
 
 extern struct CANVariables InverterState;
 
 uint16_t speed = 0;
 
 TM1637TinyDisplay speedDisplay(SPD_CLK, SPD_DATA), batteryDisplay(BATT_CLK,BATT_DATA), motorTempDisplay(TEMP_CLK,TEMP_DATA), coolantTempDisplay(COOL_CLK,COOL_DATA);
+LiquidCrystal_I2C lcd(0x27,20,4);
 
 bool GPIOHandler::Init()
 {
@@ -17,11 +26,9 @@ bool GPIOHandler::Init()
         batteryDisplay.begin();
         motorTempDisplay.begin();
         coolantTempDisplay.begin();
-        //speedDisplay.showString("NDSU SAE");
-        speedDisplay.showNumber(43.21);
-        batteryDisplay.showNumber(123.4);
-        coolantTempDisplay.showNumber(43.21);
-        motorTempDisplay.showNumber(000.0);
+        GPIOHandler::LcdInit();
+    //speedDisplay.showString("NDSU SAE");
+        //LCDDisplaySAE();
     #endif
     
     return true;
@@ -39,6 +46,41 @@ void GPIOHandler::UpdateDisplays()
     batteryDisplay.showNumber(InverterState.DC_Bus_Voltage);
     motorTempDisplay.showNumber(InverterState.Motor_Temp_1);
     coolantTempDisplay.showNumber(InverterState.Inverter_Coolant_Temp);
+}
+
+bool GPIOHandler::LcdInit()
+{
+  //Initialized screen and backlight.
+  lcd.init();
+  lcd.backlight();
+}
+
+void GPIOHandler::LCDDisplaySAE()
+{
+  //Characters used to create the logo
+  lcd.createChar(0, fullBox);
+  lcd.createChar(1, halfBoxBottom);
+  lcd.createChar(2, halfBoxTop);
+  lcd.createChar(3, topLeftAChar);
+  lcd.createChar(4, sideLeftAChar);
+  lcd.createChar(5, sideLeftAInnerChar);
+  lcd.createChar(6, sideLeftInnerCharConnector);
+  lcd.createChar(7, topRightSCurve);
+
+  delay(3000);
+  uint8_t i = 0;
+  while (1) {
+    lcd.clear();
+    lcd.print("Codes 0x"); lcd.print(i, HEX);
+    lcd.print("-0x"); lcd.print(i+16, HEX);
+    lcd.setCursor(0, 1);
+    for (int j=0; j<16; j++) {
+      lcd.printByte(i+j);
+    }
+    i+=16;
+    
+    delay(4000);
+  }
 }
 
 uint16_t GPIOHandler::GetPedalSpeed()
