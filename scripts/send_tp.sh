a7ee515aaf714c626b15bc9f4048b7ad0f38f144
Brennan Coslett <brennan@coslett.org>
2025-02-22 16:47:38 -0600

    scripts/send_tp.sh: Update to send DM1's in a loop
    This causes a cycle of FAULT UPDATE messages to show up in the serial
    console to verify our fault monitoring code is working correctly.
    
    FAULT UPDATE - SPN: 517026  FMI: 9 Occ: 125
    FAULT UPDATE - SPN: 517026  FMI: 9 Occ: 126
    FAULT UPDATE - SPN: 517026  FMI: 9 Occ: 127
    FAULT UPDATE - SPN: 517026  FMI: 9 Occ: 1
    FAULT UPDATE - SPN: 517026  FMI: 9 Occ: 2
    FAULT UPDATE - SPN: 517026  FMI: 9 Occ: 3
    FAULT UPDATE - SPN: 517026  FMI: 9 Occ: 4
    FAULT UPDATE - SPN: 517026  FMI: 9 Occ: 5

--- scripts/send_tp.sh
+++ scripts/send_tp.sh
@@ -1,11 +1,38 @@
-#!/bin/zsh
+#!/bin/bash
 
-#while true
-#do
-cansend can0 "1CECFFA2#200A0002FFCAFE00" 
-sleep 0.015
-cansend can0 "1CEBFFA2#01FFFFA7FAE81FA2"
-sleep 0.015
-cansend can0 "1CEBFFA2#02E3E9B9FFFFFFFFF"
-sleep 0.015
-#done
+# Configuration
+INTERVAL=0.005  # Time between fault messages in seconds
+MAX_OCCURRENCES=255  # Maximum number of occurrences before resetting
+OCCURRENCE_INCREMENT=1  # How much to increment occurrence each time
+
+# Initialize occurrence counter
+occurrence=1
+
+# Function to send DM1 message using transport protocol
+send_dm1() {
+    local occ=$1
+    # Convert occurrence to hex
+    local occ_hex=$(printf "%02X" $occ)
+    
+    # Send transport protocol messages in sequence
+    cansend can0 "1CECFFA2#200A0002FFCAFE00" 
+    sleep 0.001
+    cansend can0 "1CEBFFA2#01FFFFA7FAE81FA2"
+    sleep 0.001
+    cansend can0 "1CEBFFA2#02E3E9${occ_hex}FFFFFFFFF"
+    sleep 0.001
+}
+
+echo "Starting DM1 transport protocol message loop. Press Ctrl+C to stop."
+
+while true; do
+    send_dm1 $occurrence
+    
+    # Increment occurrence, reset if we hit max
+    occurrence=$((occurrence + OCCURRENCE_INCREMENT))
+    if [ $occurrence -gt $MAX_OCCURRENCES ]; then
+        occurrence=1
+    fi
+    
+    sleep $INTERVAL
+done
